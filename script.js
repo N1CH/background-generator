e6894950245f85a40d9315593fc09242d4f0ca8c
N1CH <nicachuma@gmail.com>
2021-05-03 23:53:29 -0300

    Added challenges

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+setGradient();
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
@@ -12,6 +13,21 @@ function setGradient() {
 	+ ")";
 
 	css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+  
+function setRandomColor() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
 }
 
 color1.addEventListener("input", setGradient)
